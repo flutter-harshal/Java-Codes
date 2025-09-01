b3a978a2873ae34ad1f1d7b841f2aef2b83698c6
flutter-harshal <nahireharsh@gmail.com>
2025-09-01 18:48:37 +0530

    New commit

--- src/ExceptionPro.java
+++ src/ExceptionPro.java
@@ -1,2 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ExceptionPro {
+    public static void main(String[] args) throws IOException {
+        Demo6 demo = new Demo6();
+        demo.getData();
+
+    }
+}
+
+class  Demo6 {
+    void  getData() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int data=0;
+
+        try{
+            data=Integer.parseInt(br.readLine());
+
+        }catch(NumberFormatException obj){
+            System.out.println(obj);
+
+        }
+
+        System.out.println(data);
+    }
 }
--- src/ExceptionPro1.java
+++ src/ExceptionPro1.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.util.zip.DataFormatException;
 
 public class ExceptionPro1 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the integer Value");
+
+        for(int i=0;i< arr.length;i++){
+            int data=  scanner.nextInt();
+
+            if(data<0){
+                throw  new DataUnderflowException("Data is less than 0");
+            }
+            if (data>100){
+                throw  new DataOverflowException("Data is greater than 100");
+
+            }
+            arr[i]=data;
+        }
+
+        for(int i=0;i< arr.length;i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
+
+class  DataUnderflowException extends RuntimeException{
+    DataUnderflowException(String msg){
+        super(msg);
+    }
+}
+class  DataOverflowException extends RuntimeException{
+    DataOverflowException(String msg){
+        super(msg);
+    }
 }
